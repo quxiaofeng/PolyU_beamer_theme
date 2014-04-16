aaf4ac2a32ce9be488bb32c9ac7beb3536750d3c
Xiaofeng QU <xiaofeng.qu.hk@ieee.org>
2014-04-16 12:01:29 +0800

    add a new example; tune the color
    Signed-off-by: Xiaofeng QU <xiaofeng.qu.hk@ieee.org>

--- makereleasezip.bat
+++ makereleasezip.bat
@@ -1,6 +1,7 @@
 echo off
 FOR /R "." %%s IN (*.aux) DO call del "%%s"
 FOR /R "." %%s IN (*.bak) DO call del "%%s"
+FOR /R "." %%s IN (*.bbl) DO call del "%%s"
 FOR /R "." %%s IN (*.blg) DO call del "%%s"
 FOR /R "." %%s IN (*.fdb_latexmk) DO call del "%%s"
 FOR /R "." %%s IN (*.fls) DO call del "%%s"
@@ -14,6 +15,7 @@ FOR /R "." %%s IN (*.snm) DO call del "%%s"
 FOR /R "." %%s IN (*.synctex.gz) DO call del "%%s"
 FOR /R "." %%s IN (*.tex.sav) DO call del "%%s"
 FOR /R "." %%s IN (*.toc) DO call del "%%s"
+FOR /R "." %%s IN (*.vrb) DO call del "%%s"
 FOR /R "." %%s IN (*~) DO call del "%%s"
 
 del *.zip /q
